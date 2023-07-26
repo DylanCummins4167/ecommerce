98a5f10a77ee988faf3aa0d075d7999ad08dfd42
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-07-26 02:19:21 -0500

    Update Tag.js

--- fantastic-umbrella/Develop/models/Tag.js
+++ fantastic-umbrella/Develop/models/Tag.js
@@ -6,7 +6,35 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+   // models/Tag.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
+
   },
   {
     sequelize,
